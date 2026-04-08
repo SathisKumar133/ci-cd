15780cb470385b389a02f09e72fbc8ca33c2acc9
Sathiskumar133 <sathiskumar8724@gmail.com>
2026-04-08 17:03:34 +0530

    Update ESLint configuration and install additional ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,8 @@
-import js from "@eslint/js";
-
 export default [
-    js.configs.recommended,
+    {
+        rules: {
+            "no-unused-vars": "warn",
+            "no-undef": "warn",
+        },
+    },
 ];
